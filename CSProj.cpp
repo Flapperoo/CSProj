eaa3a753e27d6e48f3450d1549545d14cfc5ab4b
Flapperoo <gianandreb@gmail.com>
2021-02-07 12:47:08 +0800

    Added moditem and seaitem

--- CSProj.cpp
+++ CSProj.cpp
@@ -15,7 +15,7 @@ struct iteminv{
 	float itemprice;
 	int itemquan;
 	string itemunit;
-} itemdb[100], temp, search[100];
+} itemdb[100], temp, display[100], result[100];
 
 void loadfile(){
 	ifstream input("itemdb.txt");
@@ -201,7 +201,7 @@ void delitem(){
 	do{
 		loadfile();
 		system("cls");
-		repeat = true;
+		repeat = false;
 		cout<<"-------------------- DELETE ITEM --------------------"<<endl<<"\nInput Item Code to Delete >> ";
 		cin>>delcode;
 		//delcode = tolower(delcode);
@@ -251,16 +251,158 @@ void delitem(){
 		cout<<"Press any key to repeat or (N/n) to return to menu >> ";
 		cin>>ch2;
 		ch2 = tolower(ch2);
-		if(ch2 == 'n'){
-			repeat = false;
+		if(ch2 != 'n'){
+			repeat = true;
 		}
+		cin.ignore(100, '\n')
 	}while(repeat);
 }
 
 void moditem(){
+	string modcode;
+	bool repeat,modrep;
+	char ch,ch2;
+
+	do{
+		repeat = false;
+		loadfile();
+		system("cls");
+		cout<<"-------------------- MODIFY ITEM --------------------"<<endl<<"\nInput Item Code to Modify >> ";
+		cin>>modcode;
+		transform(modcode.begin(), modcode.end(), modcode.begin(), ::tolower);
+		for(int i=0;i<100;i++){
+			if(modcode == itemdb[i].itemcode){
+				temp = itemdb[i];
+				do{
+					modrep = true;
+					cout<<"[1] Item Code: "<<temp.itemcode<<endl<<"[2] Item Name: "<<temp.itemname<<endl<<"[3] Item Variety: "<<temp.itemdesc<<endl<<"[4] Item Price: "<<temp.itemprice<<endl<<"[5] Item Quantity: "<<temp.itemquan<<endl<<"[6] Item Units: "<<temp.itemunit<<endl<<"\nEnter [1/2/3/4/5/6] to modify entry, [Y/y] to Confirm Changes, [N/n] to Cancel >> ";
+					cin>>ch;
+					ch = tolower(ch);
+					switch(ch){
+						case '1':
+							additem1();
+							break;
+						case '2':
+							additem2();
+							break;
+						case '3':
+							additem3();
+							break;
+						case '4':
+							additem4();
+							break;
+						case '5':
+							additem5();
+							break;
+						case '6':
+							additem6();
+							break;
+						case 'y':{						
+							itemdb[i] = temp;
+							remove("itemdb.txt");
+							ofstream output("itemdb.txt");
+							if(!output.fail()){
+								cout<<"Saving..."<<endl;
+								for(int a=0;a<100;a++){
+									if(itemdb[a].itemcode.size() == 0){
+									}
+									else{
+										output<<itemdb[a].itemcode<<"\t"<<itemdb[a].itemname<<"\t"<<itemdb[a].itemdesc<<"\t"<<itemdb[a].itemprice<<"\t"<<itemdb[a].itemquan<<"\t"<<itemdb[a].itemunit<<endl;
+									}
+								}
+							}
+							else{
+								cout<<"Process failed."<<endl;
+							}
+							modrep = false;
+							break;
+						}
+						case 'n':
+							modrep = false;
+							break;
+						default:
+							errorinp();
+							break;
+					}
+				}while(modrep);
+				break;
+			}
+			if(i == 99){
+				cout<<"Item Code not found"<<endl;
+			}
+		}
+		cout<<"Press any key to repeat or (N/n) to return to menu >> ";
+		cin>>ch2;
+		ch2 = tolower(ch2);
+		if(ch2 != 'n'){
+			repeat = true;
+		}
+		cin.ignore(100, '\n')
+	}while(repeat);
 }
 
 void seaitem(){
+	string seacode,tempcode;
+	int cnt=0,cntinner,searchcnt;
+	bool repeat;
+	char ch;
+
+	system("cls");
+	loadfile();
+	for(int i=0;i<100;i++){
+		if(itemdb[i].itemcode.size() == 0){
+		}
+		else if(itemdb[i].itemquan <= 5){
+			display[cnt] = itemdb[i];
+			cnt++;
+		}
+	}
+	for(int i=0;i<100;i++){
+		cntinner = cnt;
+		for(int a=0;a<=cntinner;a++){
+			if(itemdb[i].itemcode == display[a].itemcode){
+				break;
+			}
+			else if(a == cntinner){
+				display[cnt] = itemdb[i];
+				cnt++;
+			}
+		}
+	}
+
+	do{
+		repeat = false;
+		searchcnt = 0;
+		cout<<"-------------------- SEARCH ITEM --------------------"<<endl<<"\nItem Code // Item Name // Item Variety // Item Price // Item Quantity // Item Unit"<<endl;
+		for(int i=0;i<cnt;i++){
+			cout<<display[i].itemcode<<"\t"<<display[i].itemname<<"\t"<<display[i].itemdesc<<"\t"<<display[i].itemprice<<"\t"<<display[i].itemquan<<"\t"<<display[i].itemunit<<endl;
+		}
+		cout<<"Input Item Name to search >> ";
+		cin>>seacode;
+		transform(seacode.begin(), seacode.end(), seacode.begin(), ::tolower);
+		for(int i=0;i<cnt;i++){
+			tempcode = display[i].itemname;
+			transform(tempcode.begin(), tempcode.end(), tempcode.begin(), ::tolower);
+			if(tempcode.find(seacode) != string::npos){
+				result[searchcnt] = display[i];
+				searchcnt++;
+			}
+		}
+		system("cls");
+		cout<<"\n Found "<<searchcnt<<" items containing the string '"<<seacode<<"'."<<endl;
+		cout<<"-------------------- SEARCH ITEM --------------------"<<endl<<"\nItem Code // Item Name // Item Variety // Item Price // Item Quantity // Item Unit"<<endl;
+		for(int i=0;i<searchcnt;i++){
+			cout<<"["<<i+1<<"] "<<result[i].itemcode<<"\t"<<result[i].itemname<<"\t"<<result[i].itemdesc<<"\t"<<result[i].itemprice<<"\t"<<result[i].itemquan<<"\t"<<result[i].itemunit<<endl;
+		}
+		cout<<"\n Press any key to search again and [N/n] to return to menu >> ";
+		cin>>ch;
+		ch = tolower(ch);
+		if(ch != 'n'){
+			repeat = true;
+		}
+		cin.ignore(100, '\n')
+		system("cls");
+	}while(repeat);
 }
 
 int main(){
